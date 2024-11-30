a95378e2c52d3f044f5843b2058d627c06639cb7
SteffRhes <steffrhes@pm.me>
2024-11-30 22:53:48 +0100

    improve input and output file handling in veld_ann2standoff

--- veld_ann2standoff.sh
+++ veld_ann2standoff.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cp /veld/input/"$in_conllu_file" /veld/output/
-in_txt_file_ensured="${in_conllu_file%.conllu}.txt"
-cp /veld/input/"$in_txt_file" /veld/output/"$in_txt_file_ensured"
+in_txt_file_expected="${in_conllu_file%.conllu}.txt"
+if [ $in_txt_file != $in_txt_file_expected ]; then
+  cp /veld/input/"$in_txt_file" /veld/input/"$in_txt_file_expected"
+fi
 
-command="./ann2standoff /veld/output/${in_conllu_file}"
+command="./ann2standoff /veld/input/${in_conllu_file}"
 
 if [ -n "$in_ann2standoff_ini_file" ]; then
   command+=" -c /veld/input/${in_ann2standoff_ini_file}"
@@ -18,6 +19,14 @@ echo "executing:"
 echo "$command"
 eval "$command"
 
-rm /veld/output/"$in_conllu_file"
-rm /veld/output/"$in_txt_file_ensured"
+if [ $in_txt_file != $in_txt_file_expected ]; then
+  rm /veld/input/"$in_txt_file_expected"
+fi
+
+out_json_file_generated="${in_conllu_file%.conllu}.ann.json"
+if [ -n "$out_json_file" ]; then
+  mv /veld/input/"$out_json_file_generated" /veld/output/"$out_json_file"
+else
+  mv /veld/input/"$out_json_file_generated" /veld/output/
+fi
 
